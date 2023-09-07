7140696645916e0c5b6ae8fc7a529deacf1196ec
M-Tyshchenko <122963239+M-Tyshchenko@users.noreply.github.com>
2023-09-07 07:09:14 +0000

    Deploying to gh-pages from @ M-Tyshchenko/goit-react-hw-05-movies@4c9223fcf2462bc08a129c12a40ca9e2b7fa9b36 🚀

---
+++ static/js/417.e65580ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[417],{417:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var r,o=t(168),u=t(867).ZP.h1(r||(r=(0,o.Z)(["\n  margin: 20px auto;\n  font-size: 24px;\n  font-weight: 700;\n  display: flex;\n  justify-content: center;\n"]))),a=t(184),c=function(){return(0,a.jsx)(u,{children:"Page not found"})}}}]);
+//# sourceMappingURL=417.e65580ab.chunk.js.map
